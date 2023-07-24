75a9e9ad8f073574f802bbf3b9d31cc5b9af9a0d
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-07-24 18:02:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("squareContainer");
+  const numberOfSquares = 800;
+
+  for (let i = 0; i < numberOfSquares; i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    container.appendChild(square);
+  }
+});
