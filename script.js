2480de217e7fcaf3485712dc945cc09246987202
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-07-28 17:07:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.getElementById("squareContainer");
-  const numberOfSquares = 800;
+//your JS code here. If required.
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 
-  for (let i = 0; i < numberOfSquares; i++) {
-    const square = document.createElement("div");
-    square.className = "square";
-    container.appendChild(square);
-  }
-});
+
+
+const container  = document.getElementById("container")
+
+const SQUARES = 800
+
+for(let i=0;i<SQUARES;i++){
+    const square = document.createElement("div")
+    square.classList.add("square")
+
+    square.addEventListener("mouseover",()=> setColor(square))
+    square.addEventListener("mouseout",()=> removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element){
+    const color = randomColor()
+
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+}
+
+function removeColor(element){
+    element.style.background = "#1d1d1d"
+    element.style.boxShadow = "0 0 2px #000"
+}
+function randomColor(){
+    const index = Math.floor(Math.random()*colors.length)
+    return colors[index]
+}
